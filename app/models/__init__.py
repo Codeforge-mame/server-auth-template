c82203ca5f375f5200532a17457dc4d2c734175b
Mohammed Ahmed <alidevyou@gmail.com>
2026-08-27 10:19:27 +0300

    Update __all__ export in models init file
    Removed Chat and Message models from __all__ export.

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,5 @@
 # app/models/__init__.py
 from app.core.db import Base
 from app.models.user import User
-from app.models.chat import Chat
-from app.models.message import Message
 
 __all__ = ["Base", "User"]
