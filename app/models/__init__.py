76f3a136099482760f64aead54fbd8ef53ebe177
Mohammed Ahmed <alidevyou@gmail.com>
2026-08-27 10:19:12 +0300

    Remove Message from __all__ in models init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -4,4 +4,4 @@ from app.models.user import User
 from app.models.chat import Chat
 from app.models.message import Message
 
-__all__ = ["Base", "User", "Chat", "Message"]
+__all__ = ["Base", "User"]
